3026620d3ffa595561c759d85b9dde9f45d32292
Shweta <shwetasinghshweta@gmail.com>
2023-12-16 00:59:19 +0530

    initial commit

---
+++ assets/js/image-widget.js
@@ -0,0 +1,21 @@
+document.addEventListener('DOMContentLoaded', function(){
+    var images = [
+        '/assets/images/000000580410_couch (1).jpg',
+        '/assets/images/000000138954_apple.jpg',
+        '/assets/images/000000368982_scissors.jpg',
+        '/assets/images/000000002157_dining table.jpg',
+    ];
+
+    var imageIndex = 0;
+    var imageElement = document.getElementById('widget-image');
+
+    function changeImage(){
+        imageElement.src = images[imageIndex];
+        imageIndex = (imageIndex+1) % images.length;
+    }
+
+    changeImage();
+
+    setInterval(changeImage,2000);
+
+});
