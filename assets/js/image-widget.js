2e79cf8eebff23edef5d29aa73d739795ad24f0a
Shweta <shwetasinghshweta@gmail.com>
2023-12-16 03:47:24 +0530

    image path

--- assets/js/image-widget.js
+++ assets/js/image-widget.js
@@ -1,21 +1,21 @@
 document.addEventListener('DOMContentLoaded', function(){
     var images = [
-        '/assets/images/000000580410_couch (1).jpg',
-        '/assets/images/000000138954_apple.jpg',
-        '/assets/images/000000368982_scissors.jpg',
-        '/assets/images/000000002157_dining table.jpg',
+        '000000580410_couch (1).jpg',
+        '000000138954_apple.jpg',
+        '000000368982_scissors.jpg',
+        '000000002157_dining table.jpg',
     ];
 
     var imageIndex = 0;
     var imageElement = document.getElementById('widget-image');
     var baseurl = imageElement.getAttribute('data-baseurl');
+
     function changeImage(){
-        imageElement.src = images[imageIndex];
+        imageElement.src = baseurl + '/assets/images/' + images[imageIndex];
         imageIndex = (imageIndex+1) % images.length;
     }
 
     changeImage();
 
-    setInterval(changeImage,2000);
-
+    setInterval(changeImage, 2000);
 });
